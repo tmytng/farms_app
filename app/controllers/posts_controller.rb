841a1a97ccab7c31257ffc6f250a23260d1a37f9
tmytng <tmytng.2020@gmail.com>
2020-11-05 23:11:43 +0900

    controller file update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,7 +15,21 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
-    # if @post.save
-    #   flas
+    if @post.save
+      flash[:notice] = '新規登録が完了しました'
+      redirect_to project_path(@project)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:company_name, :company_address, :company_url, :prefecture_id, :contact_person, :contact_reason,:phone_number, :email, :leadstatus_id, :purchase_date, :product_id, :contact_des, :add_file, :project_id).merge(user_id: current_user.id)
+  end
+
+  def set_project
+    @project = Project.find(params[:project_id])
   end
 end
