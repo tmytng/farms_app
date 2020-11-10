aeacdcf878ef927f18c148e4026b57ce0f33867d
tmytng <tmytng.2020@gmail.com>
2020-11-10 18:32:53 +0900

    fix project-create

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,5 @@
 class ProjectsController < ApplicationController
-  before_action :set_project, only: [:create, :update, :edit, :show]
+  before_action :set_project, only: [:update, :edit, :show]
 
   def index
     @projects = Project.all
@@ -7,11 +7,11 @@ class ProjectsController < ApplicationController
 
   def new
     @project = Project.new
-    # @project.users << current_user
   end
 
   def create
-    if @project.save
+    @project = Project.new(project_params)
+    if @project.save!
       redirect_to projects_path, notice: '新規プロジェクトを作成しました'
     else
       render :new
@@ -42,6 +42,7 @@ class ProjectsController < ApplicationController
   end
 
   def project_params
-    params.require(:project).permit(:name, :project_image, :profile, user_ids: []).merge(user_id: current_user.id)
+    params.require(:project).permit(:name, :profile, :prj_image, user_ids: [])
+    # .merge(user_id: current_user.id)
   end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,9 +1,11 @@
 class Project < ApplicationRecord
-  has_many :project_users
+  has_many :project_users, dependent: :destroy
   has_many :posts
   has_many :users, through: :project_users
   has_many :messages
-  has_one_attached :project_image
+  has_one_attached :prj_image
   validates :name, presence: true, uniqueness: true
+  validates :profile, presence: true, uniqueness: true
+  validates :project_user_ids, presence: true
 
 end
