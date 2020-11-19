2f0842c941e330fa74b5847f7012c9b9228b2f06
tmytng <tmytng.2020@gmail.com>
2020-11-19 20:44:45 +0900

    bootstrap test

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,27 +1,2 @@
-<template>
-  <div>
-    <b-form-checkbox
-      id="checkbox-1"
-      v-model="status"
-      name="checkbox-1"
-      value="accepted"
-      unchecked-value="not_accepted"
-    >
-      I accept the terms and use
-    </b-form-checkbox>
+<script></script>
 
-    <div>
-      State: <strong>{{ status }}</strong>
-    </div>
-  </div>
-</template>
-
-<script>
-export default {
-  data() {
-    return {
-      status: "not_accepted",
-    };
-  },
-};
-</script>
