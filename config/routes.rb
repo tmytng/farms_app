d069b8621befc3b6a9a864e6fd759377ac7e664c
tmytng <tmytng.2020@gmail.com>
2020-11-19 16:20:25 +0900

    bootstrap test

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root "projects#index"
-  resources :users
+  resources :users do
+    collection do
+      get :test
+    end
+  end
   resources :projects do
     resources :posts, :messages
   end
