dfdefff40817520689c95bf6474e2ce94c87652a
tmytng <tmytng.2020@gmail.com>
2020-11-25 18:38:02 +0900

    fix user-validation

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
 
   factory :user do
-    name              {"abe"}
+    name              {"testuser"}
     email                 {"kkk@gmail.com"}
     password              {"00000000"}
     password_confirmation {"00000000"}
