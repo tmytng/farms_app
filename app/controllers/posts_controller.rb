ffbe8d761c5731e960e74a647a7a124f15a9538f
tmytng <tmytng.2020@gmail.com>
2020-11-27 03:34:15 +0900

    fix-post-new

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,11 +21,12 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @logs = @post.audits.all
   end
 
   def update
     if @post.update(post_params)
-      redirect_to project_posts_path, notice: '登録情報を修正しました'
+      redirect_to project_post_path, notice: '登録情報を修正しました'
     else
       render :edit, notice: '登録情報が修正できません'
     end
