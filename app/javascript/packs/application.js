e002808674afef4c1cac19d2486680da0929c304
tmytng <tmytng.2020@gmail.com>
2020-11-27 19:59:50 +0900

    delete Data-Confirm Modal

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,6 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("data-confirm-modal")
-
 // require("chartkick")
 // require("chart.js")
 
