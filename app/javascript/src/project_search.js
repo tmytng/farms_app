7f3c28c1dc9202ca63021457b2a1ea525672febe
tmytng <tmytng.2020@gmail.com>
2020-12-10 20:37:09 +0900

    change layout pj-index

--- app/javascript/src/project_search.js
+++ app/javascript/src/project_search.js
@@ -1,4 +1,23 @@
 $(function() {
+
+    let search_list = $(".pj-box");
+
+    function appendProject(project) {
+
+        let html =
+            `<div class="pj-box">
+              <h1>test</h1>
+              <p>${project.name}</p>
+              <p>${project.profile}</p>
+            </div>`
+        search_list.append(html);
+    }
+
+    function appendErrMsgToHTML(msg) {
+        let html = `<div class='name'>${ msg }</div>`
+        search_list.append(html);
+    }
+
     $(".search-input").on("keyup", function() {
         let input = $(".search-input").val();
         $.ajax({
@@ -8,17 +27,14 @@ $(function() {
                 dataType: 'json'
             })
             .done(function(projects) {
-                $(".index_field").empty();
+                search_list.empty();
                 if (projects.length !== 0) {
                     projects.forEach(function(project) {
-                        appendTweet(project);
+                        appendProject(project);
                     });
                 } else {
-                    appendErrMsgToHTML("一致するプロジェクトがありません");
+                    appendErrMsgToHTML("一致するツイートがありません");
                 }
             })
-            .fail(function() {
-                alert('error');
-            });
     });
 });
